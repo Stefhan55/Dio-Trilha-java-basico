4bd308984fe26899719f707fb4ea937a7a3410f8
Diuliano <diulianosmarquesmendes@gmail.com>
2025-02-20 19:13:08 -0300

    ÇÇÇ

---
+++ src/operadores/operadores.java
@@ -0,0 +1,12 @@
+package operadores;
+
+public class operadores {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        System.out.println(-numero);
+
+        System.out.println(numero);
+
+    }
+}
---
+++ src/tiposdeVariaveis/Tiposdevariaveis.java
@@ -0,0 +1,12 @@
+package tiposdeVariaveis;
+
+
+
+public class Tiposdevariaveis {
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+    }
+}
